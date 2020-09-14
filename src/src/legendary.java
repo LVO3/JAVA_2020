1ef058a65f20abd0e50045228187d589fa9b54f2
LVO3 <t1229y@naver.com>
2020-09-14 21:07:58 +0900

    가위바위보 1차 현재 오후 09시 07분 재밌다

--- src/src/legendary.java
+++ src/src/legendary.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 public class legendary {
     public static void main(String[] args) {
-        String str = new String("안녕하세요");
-        System.out.println(str);
+        System.out.println("가위 바위 보 게임");
+        System.out.println("가위:1, 바위:2, 보:3");
+
+        Scanner sc = new Scanner(System.in);
+        int player = sc.nextInt();
+
+        double randomValue = Math.random();
+        int intValue = (int)(randomValue * 3) + 1;
+
+        System.out.println("플레이어 : " + player);
+        System.out.println("컴퓨터 : " + intValue);
     }
 }
